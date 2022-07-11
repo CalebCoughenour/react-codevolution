8ca25221844dd739faaee3e6dc7dfa53f5d5b490
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-11 16:13:02 -0700

    add initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Components/Greet'
+import Welcome from './Components/Welcome';
+import Hello from './Components/Hello';
+import Message from './Components/Message';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message/>
+      {/* <Hello /> */}
+      {/* <Greet name="Bruce" heroName="Batman"><p>This is children props</p></Greet>
+      <Greet name="Sarah" heroName="spiderwoman">
+        <button>enter</button>
+      </Greet>
+      <Greet name="Rachel" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman" />
+      <Welcome name="Diana" heroName="Wonder Woman" /> */}
     </div>
   );
 }
