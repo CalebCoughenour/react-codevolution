8a0ca105553e6258e0e0a8d7b616a27db4fdd9d5
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-12 15:31:35 -0700

    add index key

--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -3,6 +3,8 @@ import Person from './Person'
 
 function NameList() {
 
+  const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
+
   const persons = [
     {
       id: 1,
@@ -24,8 +26,8 @@ function NameList() {
     }
   ]
 
-  const personList = persons.map(person => <Person key={person.id} person={person} />)
-  return <div>{personList}</div>
+  const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+  return <div>{nameList}</div>
 
 
   // const names = ['Bruce', 'Clark', 'Diana']
