a77db4fa3183877c2a8b466c57bfd37c22446618
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-12 11:27:56 -0700

    add method through prop. method in parent passed as prop to child

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import Welcome from './Components/Welcome';
 import Hello from './Components/Hello';
 import Message from './Components/Message';
 import Counter from './Components/Counter';
+import FunctionClick from './Components/FunctionClick';
+import ClassClick from './Components/ClassClick';
+import EventBind from './Components/EventBind';
+import ParentComponent from './Components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
+      <ParentComponent />
+      {/* <ClassClick /> */}
+      {/* <FunctionClick /> */}
+      {/* <EventBind /> */}
       {/* <Message/> */}
-      <Counter />
+      {/* <Counter /> */}
       {/* <Hello /> */}
       {/* <Greet name="Bruce" heroName="Batman"><p>This is children props</p></Greet>
       <Greet name="Sarah" heroName="spiderwoman">
         <button>enter</button>
-      </Greet>
-      <Greet name="Rachel" heroName="Wonder Woman" />
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
+      </Greet> */}
+      {/* <Greet name="Rachel" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman" /> */}
+      {/* <Welcome name="Clark" heroName="Superman" />
       <Welcome name="Diana" heroName="Wonder Woman" /> */}
     </div>
   );
